6ad826c3bb7513ea28c5caa35cd34d62aa336006
ZedXter <digaleev@gmail.com>
2011-05-19 10:27:13 +0600

    нашёл готовый css файлик с дизайном как у нас, добавил представление модели картинки в админке

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,9 @@
-
 from main.models import Image, Group
 from django.contrib import admin
 
-admin.site.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'pub_date', 'group')
+    list_filter = ('group', 'pub_date')
+
+admin.site.register(Image, ImageAdmin)
 admin.site.register(Group)
