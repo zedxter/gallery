e6f90c3fabf0e46d75ef9cfc04894843ac864db9
Dan <digaleev@gmail.com>
2011-05-22 23:10:16 +0600

    Подправил багу с ошибкой загрузки страницы

--- main/views.py
+++ main/views.py
@@ -3,6 +3,5 @@ from django.shortcuts import render_to_response
 from main.models import Image, Group
 
 def index(request):
-    image = Image.objects.get(pk=1)
     groups = Group.objects.all()
-    return render_to_response('index.html', {'image': image, 'groups': groups})
+    return render_to_response('index.html', {'groups': groups})
