da3d819d91d26cb1293fc1f297f0154455a63222
ZedXter <digaleev@gmail.com>
2011-07-04 17:32:18 +0600

    убрал ненужные линки в админке

--- main/admin.py
+++ main/admin.py
@@ -1,9 +1,16 @@
 from main.models import Image, Group
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.sites.models import Site
+from django.contrib.auth.models import Group as Gr
 
 class ImageAdmin(admin.ModelAdmin):
     list_display = ('title', 'pub_date', 'group')
     list_filter = ('group', 'pub_date')
 
 admin.site.register(Image, ImageAdmin)
-admin.site.register(Group)
+admin.site.register(Gr)
+
+admin.site.unregister(User)
+admin.site.unregister(Group)
+admin.site.unregister(Site)
